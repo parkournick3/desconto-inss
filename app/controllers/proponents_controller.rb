9c6264ecc3c9f59aff107dd1a670c9c804948af9
nicolas <nicolasalexandre0001@gmail.com>
2024-03-29 15:57:00 -0300

    style: add navbar

--- app/controllers/proponents_controller.rb
+++ app/controllers/proponents_controller.rb
@@ -46,8 +46,8 @@ class ProponentsController < ApplicationController
     @proponent.destroy
 
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@proponent)}_container") }
-      format.html { redirect_to todos_url, notice: 'Proponent was successfully destroyed' }
+      # format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@proponent)}_container") }
+      format.html { redirect_to proponents_url, notice: 'Proponent was successfully destroyed' }
       format.json { head :no_content }
     end
   end
