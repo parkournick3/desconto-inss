23a4c52b0ab7a5a124ad260f18358a1ec07a6ebd
nicolas <nicolasalexandre0001@gmail.com>
2024-03-29 21:01:23 -0300

    feat: add proponents by salary range chart

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,42 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  def index
+  def index # rubocop:disable Metrics/MethodLength
     @proponents_by_salary_range = Proponent.proponents_count_by_salary_range
+
+    @chart_type = 'bar'
+
+    @chart_data = {
+      labels: @proponents_by_salary_range.keys,
+      datasets: [
+        {
+          label: 'Quantidade de proponentes',
+          data: @proponents_by_salary_range.values,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+          borderWidth: 1
+        }
+      ]
+    }
+
+    @chart_options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
   end
 end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,6 +3,8 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application";
+import Chartjs from "@stimulus-components/chartjs";
 
 import ProponentsController from "./proponents_controller";
 application.register("proponents", ProponentsController);
+application.register("chartjs", Chartjs);
