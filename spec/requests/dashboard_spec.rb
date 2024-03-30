3b8a181be85a274c6c03b8e6ae1aad9321468c15
nicolas <nicolasalexandre0001@gmail.com>
2024-03-29 22:36:37 -0300

    test: fix dashboard index request test

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Dashboards', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/dashboard/index'
+      get '/dashboard'
       expect(response).to have_http_status(:success)
     end
   end
